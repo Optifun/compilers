c659651c2533b7622f08504598d9bcd19a2ffb34
Dmitry Frolov <frdima19@gmail.com>
2022-01-17 17:18:05 +0700

    Update App: add menu options

--- App/Program.fs
+++ App/Program.fs
@@ -2,15 +2,31 @@ open System
 open System.IO
 open Shared.Expressions
 
-[<EntryPoint>]
-let main argv =
-    let filePath = argv |> Array.item 0
-    let reader = new StreamReader(filePath)
-    let text = reader.ReadToEnd()
-
+let printLexems text =
     parseLexems text
     |> function
         | Result.Ok x -> x |> List.map (fun l -> printf "%s " l) |> ignore
         | Result.Error e -> printfn "Error %O" e
+
+[<EntryPoint>]
+let main argv =
+    let sourceCode =
+        argv
+        |> Array.tryItem 0
+        |> function
+            | Some filePath -> new StreamReader(filePath)
+            | None -> failwith "Error: require path to a file as cmd argument"
+        |> fun reader -> reader.ReadToEnd()
+
+    let mutable breakLoop = false
+
+    while not breakLoop do
+        [ ""; "[0] Exit"; "[1] Print Lexems"; "[2] Print AST" ] |> List.map (printfn "%s") |> ignore
+
+        Console.ReadLine()
+        |> function
+            | "0" -> breakLoop <- true
+            | "1" -> printLexems sourceCode
+            | _ -> breakLoop <- false
 
     0
