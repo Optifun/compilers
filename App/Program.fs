ff7c0380e96a2e033f910eaaf1d8ae7298cdfc8d
Dmitry Frolov <frdima19@gmail.com>
2022-01-24 00:05:42 +0700

    Update program: add hashset searching and printing

--- App/Program.fs
+++ App/Program.fs
@@ -1,5 +1,7 @@
 open System
 open System.IO
+open Shared
+open Shared
 open Shared.Expressions
 open FParsec
 
@@ -13,6 +15,14 @@ let printLexems text =
     |> function
         | Result.Ok x -> x |> List.iter printLexem
         | Result.Error e -> printfn "Error %O" e
+
+let asIdentifier (lexem: LexemType) =
+    lexem.asExpression
+    |> function
+        | Some (Identifier i) -> Some i
+        | _ -> None
+
+let searchIdentifier (hashSet: string HashSet) (id: string) = hashSet.has <| id
 
 let printProgram program = program.Statements |> List.iter (printf "%O ")
 
@@ -35,23 +45,41 @@ let main argv =
         argv
         |> Array.tryItem 0
         |> function
-            | Some filePath -> new StreamReader(filePath)
-            | None -> failwith "Error: require path to a file as cmd argument"
+            | Some path -> new StreamReader(path)
+            | None -> Console.ReadLine() |> fun path -> new StreamReader(path)
         |> fun reader -> reader.ReadToEnd()
 
     let mutable breakLoop = false
 
-//    test expressionParser "abc > 23"
-//    test ifThenElse "if abc > 23 then ... else if a=2 then ... ;;"
+    let hashSet = HashSet<string>(100)
+
+    let search =
+        searchIdentifier hashSet
+        >> function
+            | Some value -> printfn "%O" value
+            | _ -> printfn "Not found"
+
+    parseLexems sourceCode
+    |> function
+        | Result.Error e -> printfn "Error %O" e
+        | Result.Ok x ->
+            x
+            |> List.map asIdentifier
+            |> List.iter
+                (function
+                | Some x -> hashSet.add x
+                | _ -> ())
 
     while not breakLoop do
-        [ ""; "[0] Exit"; "[1] Print Lexems"; "[2] Print AST" ] |> List.iter (printfn "%s")
+        [ ""; "[0] Exit"; "[1] Print Lexems"; "[2] Print identifiers"; "[3] Search identifier"; "[4] Print AST" ] |> List.iter (printfn "%s")
 
         Console.ReadLine()
         |> function
             | "0" -> breakLoop <- true
             | "1" -> printLexems sourceCode
-            | "2" -> printAST sourceCode
+            | "2" -> hashSet.Print()
+            | "3" -> search <| Console.ReadLine()
+            | "4" -> printAST sourceCode
             | _ -> breakLoop <- false
 
     0
