5e66376cba8a0e439f0905b0ad889d8fe518087d
Optifun <frdima19@gmail.com>
2022-02-12 17:35:21 +0700

    Add Mini C projects

---
+++ MiniC.Core/AST.fs
@@ -0,0 +1,82 @@
+﻿module MiniC.Core.AST
+
+
+type Literal =
+    | IntNumber of Value: int
+    | FloatNumber of Value: float
+    | Boolean of Value: bool
+
+type TypeL =
+    | IntL
+    | FloatL
+    | BoolL
+    | VoidL
+
+and ArrayL = TypeL * int option
+
+and TypeLiteral =
+    | TypeL
+    | ArrayL
+
+
+type Identifier =
+    | Variable of Variable
+    | Parameter of Parameter
+    | Function of Function
+
+and Scope =
+    | Global
+    | Function of Function
+
+and Function =
+    { Parameters: Parameter list
+      ReturnType: TypeLiteral
+      Name: string }
+
+and Parameter =
+    { TypeDecl: TypeLiteral
+      Name: string
+      Function: Function }
+
+and Variable =
+    { TypeDecl: TypeLiteral
+      Name: string
+      Scope: Scope }
+
+
+type Expression =
+    | Empty
+    | Assignment of Identifier * Expression
+    | Binary of BinaryOp * Expression * Expression
+    | Literal of Literal
+    | Identifier of Identifier
+
+and BinaryOp =
+    | ConditionalOr
+    | Equal
+    | NotEqual
+    | LessEqual
+    | Less
+    | GreaterEqual
+    | Greater
+    | ConditionalAnd
+    | Add
+    | Subtract
+    | Multiply
+    | Divide
+    | Modulus
+    override x.ToString() =
+        match x with
+        | ConditionalOr -> "||"
+        | Equal -> "=="
+        | NotEqual -> "!="
+        | LessEqual -> "<="
+        | Less -> "<"
+        | GreaterEqual -> ">="
+        | Greater -> ">"
+        | ConditionalAnd -> "&&"
+        | Add -> "+"
+        | Subtract -> "-"
+        | Multiply -> "*"
+        | Divide -> "/"
+        | Modulus -> "%"
