5ce143dcb76619200583c99739a8877ac510c0c9
Optifun <frdima19@gmail.com>
2022-02-13 22:55:27 +0700

    Fix tests + add new

--- MiniC.Core.Tests/LexemParsing.fs
+++ MiniC.Core.Tests/LexemParsing.fs
@@ -8,29 +8,51 @@ open MiniC.Core.Parsers
 open MiniC.Core.Combinators
 
 
-let runParser input parser =
-    runParserOnString parser "" "some" input
+let runParser input parser = runParserOnString parser "" "some" input
 
 let mapParserResult<'a, 'b> (f: 'a -> unit) (value: ParserResult<'a, 'b>) : unit =
     value
     |> function
         | Success (v, _, _) -> f v
-        | Failure (x, e, _) -> Assert.Fail(x.ToString())
+        | Failure (x, _, _) -> Assert.Fail(x.ToString())
 
 
 [<Test>]
-let ``Parse boolean literal`` () =
+let ``Parse 'bool' literal`` () =
     let input = "bool"
     let parser = typeLexemParser TypeL.BoolL
 
     runParser input parser
     |> mapParserResult (fun v -> v |> should equal TypeL.BoolL)
 
+[<Test>]
+let ``Parse 'void' literal`` () =
+    let input = "void"
+    let parser = typeLexemParser TypeL.VoidL
+
+    runParser input parser
+    |> mapParserResult (fun v -> v |> should equal TypeL.VoidL)
 
 [<Test>]
-let ``Type lexem parser parsers all types`` () =
+let ``Parse 'int' literal`` () =
+    let input = "int"
+    let parser = typeLexemParser TypeL.IntL
+
+    runParser input parser
+    |> mapParserResult (fun v -> v |> should equal TypeL.IntL)
+
+
+[<Test>]
+let ``Type lexem parser parsers all type literals`` () =
     let input = "int void bool float"
     let parser = sepEndBy1 typeLexemCombinator <| ws
 
+    let expected =
+        [TypeL.IntL
+         TypeL.VoidL
+         TypeL.BoolL
+         TypeL.FloatL]
+        |> List.map TypeLexem
+
     runParser input parser
-    |> mapParserResult (fun v -> v |> should haveLength 4)
+    |> mapParserResult (fun v -> v |> should equal expected)
