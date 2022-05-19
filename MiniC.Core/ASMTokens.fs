66071f9bf8b22ba4dd9f0d164c5a65ff47e298b4
Optifun <frdima19@gmail.com>
2022-05-19 21:02:30 +0700

    Add Assembler translator prototype

---
+++ MiniC.Core/ASMTokens.fs
@@ -0,0 +1,44 @@
+﻿module MiniC.Core.ASMTokens
+
+type Identifier = string
+
+type Register =
+    | AX
+    | BX
+    | CX
+    | DX
+    | CI
+
+    static member all () = [ AX; BX; CX; DX; CI ]
+
+type Variable = Identifier
+
+type Literal =
+    | HexInt of int
+    | HexFloat of double
+    | Bool of bool
+
+
+type StackOperand =
+    | Register of Register
+    | Literal of Literal
+
+type ValueHolder =
+    | Variable of Variable
+    | Register of Register
+
+type Value =
+    | Register of Register
+    | Variable of Variable
+    | Literal of Literal
+
+type AToken =
+    | Call of Identifier
+    | Push of StackOperand
+    | Pop of Register
+    | Mov of ValueHolder * Value
+    | Label of Identifier
+    | FunctionBlock of FunctionBlock
+    | Return of StackOperand
+
+and FunctionBlock = AToken list
---
+++ MiniC.Core/ASMTranslator.fs
@@ -0,0 +1,94 @@
+﻿module MiniC.Core.ASMTranslator
+
+open FSharpPlus
+open FSharpPlus.Data
+open MiniC.Core.ASMTokens
+open MiniC.Core.AST
+
+let rec collectVariables statements : ASMTokens.Variable list =
+    statements
+    |> List.choose (
+        function
+        | VarDeclaration var -> Some [ var.Name ]
+        | Initialization (var, _) -> Some [ var.Name ]
+        | FuncDeclaration (head, body) ->
+            let parameters =
+                head.Parameters |> List.map (fun p -> p.Name)
+
+            let localVariables = collectVariables body
+            Some <| parameters @ localVariables
+        | _ -> None
+    )
+    |> List.collect id
+
+
+let translateCall (func: FunctionCall) (returnType: TypeLiteral) : AToken list =
+    let goto = [ Label func.FuncName ]
+
+    let saveRegisters =
+        Register.all () |> List.map (StackOperand.Register >> Push)
+
+    let loadRegisters = Register.all () |> List.map Pop
+
+    let pushLiteral =
+        List.singleton << Push << StackOperand.Literal
+
+    let pushIdentifier id =
+        [ Mov(ValueHolder.Register AX, Variable id)
+          Push(StackOperand.Register AX) ]
+
+    let passArguments =
+        func.Arguments
+        |> List.collect (
+            function
+            | Literal (Boolean b) -> pushLiteral <| Bool b
+            | Literal (IntNumber i) -> pushLiteral <| HexInt i
+            | Literal (FloatNumber f) -> pushLiteral <| HexFloat f
+            | Identifier id -> pushIdentifier id
+            | _ -> failwith "not implemented"
+        )
+
+    let getResult =
+        if returnType <> TypeLiteral.VoidL then
+            [ Pop DX ]
+        else
+            []
+
+    saveRegisters @ passArguments @ goto @ getResult @ loadRegisters
+
+
+let translateFunction (func: Function) (statementSolver: Statement -> AToken list) : AToken list =
+    let head, body = func
+    let label = Label head.Name
+
+
+    let popParameters =
+        head.Parameters
+        |> List.rev
+        |> List.collect (fun p ->
+            [ Pop DX
+              Mov(ValueHolder.Variable p.Name, Register DX) ]
+        )
+
+    let instructions =
+        body |> List.collect statementSolver
+
+    [ label
+      FunctionBlock <| popParameters @ instructions ]
+
+let rec translateStatement =
+    function
+    | Expression (Call func) -> failwith "not implemented"
+    | Initialization i -> failwith "not implemented"
+    | VarDeclaration var -> []
+    | FuncDeclaration func -> translateFunction func translateStatement
+    | st -> failwith "not allowed"
+
+let programTranslator statements globalScope : AToken list * ASMTokens.Variable list =
+    let variables = collectVariables statements
+
+    let tokens =
+        statements |> List.collect translateStatement
+
+
+    tokens, variables
